87796536f640964015a235e3e0842acaab99a6d8
Luke Sorensen <lsorensen@wustl.edu>
2016-09-12 10:11:18 -0500

    dd

--- calendarMaker.py
+++
@@ -1,7 +0,0 @@
-for x in range(1,50):
-    if(x<26):
-        print "-" + str(x*0.01) + "em " + str(x*.014) + "em rgb(" + str(x) +",255," + str(x) + "),"
-    else:
-        print "-" + str(x*0.01) + "em " + str(x*.014) + "em rgb(255,255,255),"
-        
-    
